25574790112d81135f871c5a12e690bd9af10540
mudas2q <mudasirwindows@gmail.com>
2025-12-11 14:31:09 +0500

    (Do While Loop) atm pin new add code

--- My_Personal_extra _Collection/JavaScript LOOPS/DO-WHILE_1_pin-enter.js
+++ My_Personal_extra _Collection/JavaScript LOOPS/DO-WHILE_1_pin-enter.js
@@ -1,4 +1,7 @@
+//===================================
 // 1.( DO WHILE LOOP ) -Check Pin---.
+//===================================
+
 let pin = 123;
 let user;
 do {
@@ -8,3 +11,21 @@ do {
 console.log("Correct Pin Access Granted ✅");
 alert("Correct Pin Access Granted ✅");
 document.writeln(`Correct Pin ${pin} Access Granted ✅`);
+
+//===============================================
+// 2. [SECOND METHOD ATM PIN ] ( DO WHILE LOOP )---
+//===============================================
+
+const correctPIN = "1234"; 
+let enteredPIN;
+
+do {
+    enteredPIN = prompt("Enter your ATM PIN:");
+
+    if (enteredPIN === correctPIN) {
+        alert("PIN correct. Access granted.");
+        break;
+    } else {
+        alert("Incorrect PIN. Try again.");
+    }
+} while (true); // loop tab tak chalega jab tak correct PIN nahi milta
